ccbb935750d4eddf30eb225f26f5880ea753bda5
Patrick Barrett <patrickbarrett@exosite.com>
2016-08-05 11:38:25 -0500

    use token for fetching svc docs

--- fetch_svc_docs.sh
+++ fetch_svc_docs.sh
@@ -4,6 +4,9 @@ echo "-- get service docs --"
 # check environment variable
 : ${PEGASUSAPI?"Need to set env var PEGASUSAPI to e.g. pegasus-api-dev.hosted.exosite.io or localhost:8080"}
 
+# check environment variable
+: ${PEGASUSTOKEN?"Need to set env var PEGASUSTOKEN"}
+
 if cd murano/services; then
     rm ./*
 else
@@ -11,7 +14,7 @@ else
     exit 1
 fi
 
-wget --no-verbose http://${PEGASUSAPI}/api/v1/service/doc.md -O README.md || exit 1 
+wget --no-verbose http://${PEGASUSAPI}/api/v1/service/doc.md?token=${PEGASUSTOKEN} -O README.md || exit 1
  
 # remove erroneous lines coming out of pegasus generated docs
 # once that's fixed this can be removed
@@ -20,6 +23,6 @@ grep -v '\[[0-9]*\]' ./README.md | grep -v '_meta_schema.md' > README.fixed.md ;
 while read line; do
     if [[ $line =~ \.\/([a-z0-9]+)\.md ]]; then
         echo "Getting doc for Service: ${BASH_REMATCH[1]}"
-        wget --no-verbose http://${PEGASUSAPI}/api/v1/service/${BASH_REMATCH[1]}/doc.md -O ${BASH_REMATCH[1]}.md || exit 1
+        wget --no-verbose http://${PEGASUSAPI}/api/v1/service/${BASH_REMATCH[1]}/doc.mdtoken=${PEGASUSTOKEN} -O ${BASH_REMATCH[1]}.md || exit 1
     fi
 done <README.md
